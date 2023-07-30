e50715eb0bcbec77c4048afe4a61d5a6b7047bc2
GL Direct <140243656+gldirect@users.noreply.github.com>
2023-07-31 06:20:38 +0800

    Delete functiontest.js

--- functiontest.js
+++
@@ -1,8 +0,0 @@
-function redirect() {
-      window.location.replace("https://gladyslacia.github.io");
-    }
-
-    // Check if the URL is non-existent and redirect if needed
-    if (window.location.href === "https://gldirect.github.io/document") {
-      redirect();
-    }
