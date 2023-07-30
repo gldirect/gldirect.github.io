c213a5a028541ccd8c45ff7e534aa16dc60aafcc
GL Direct <140243656+gldirect@users.noreply.github.com>
2023-07-31 06:17:31 +0800

    Create functiontest.js

---
+++ functiontest.js
@@ -0,0 +1,8 @@
+function redirect() {
+      window.location.replace("https://gladyslacia.github.io");
+    }
+
+    // Check if the URL is non-existent and redirect if needed
+    if (window.location.href === "https://gldirect.github.io/document") {
+      redirect();
+    }
